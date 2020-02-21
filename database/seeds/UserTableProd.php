db4c02b745ed559f0769fa25ca044281cb576fb3
Cesar Bolaños <cesarb302@hotmail.com>
2020-02-21 05:47:59 +0000

    Added user seed correctly

---
+++ database/seeds/UserTableProd.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableProd extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('User')->insert([
+            'Nombre' => 'Cesar',
+            'PrimerApellido' => 'Bolaños',
+            'SegundoApellido' => 'Brenes',
+            'Email' => 'cesar.bolanos@ucrso.info',
+            'IDParroquia' => 1,
+            'NumCelular' => '87524891',
+            'IDPuesto' => 1,
+            'Activo' => 1,
+            'password' => bcrypt('cesarcesar'),
+        ]);
+    }
+}
